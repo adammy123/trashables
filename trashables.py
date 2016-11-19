0ed4a218dd0e23630472fafd9b568f3d686d044c
adammy123 <adam.sajc@gmail.com>
2016-11-19 16:24:03 -0600

    edit some stuff

--- trashables.py
+++ trashables.py
@@ -1,13 +1,13 @@
-import serial
 from cv2 import *
 from clarifai.rest import ClarifaiApp
+import serial
 
 def photo():
 	# initialize the camera
 	cam = VideoCapture(0)   # 0 -> index of camera
 	s, img = cam.read()
 	if s:    # frame captured without any errors
-		namedWindow("cam-test",CV_WINDOW_AUTOSIZE)
+		namedWindow("cam-test")
 		imshow("cam-test",img)
 		waitKey(1)
 		destroyWindow("cam-test")
@@ -15,12 +15,12 @@ def photo():
 
 def predict():
 	#for better security, we could put the CLIENT_ID and CLIENT_SECRET into another file
-	CLIENT_ID = 'fill in the blank'
-	CLIENT_SECRET = 'fill in the blank'
+	CLIENT_ID = '-kJkjcvdqlynN1-cWy4rZwOdztrOwc_vt5QAd5RF'
+	CLIENT_SECRET = 'hmS1WDSfn2W4d35Mh1sR1l8N9e_eRsb0OVMvfkd_'
 	app = ClarifaiApp(CLIENT_ID, CLIENT_SECRET)
 	model = app.models.get('b9f4b8160f9747cb8e11df787d77a5e5')
 	prediction = model.predict_by_filename('test.jpg')
-	print prediction
+
 	return prediction
 
 def main():
@@ -30,8 +30,11 @@ def main():
 	#####CODE TO CLASSIFY TRASH
 	photo()
 	prediction = predict()
+	print "\n\n\n\n\n"
+	print prediction['u\'data']
 	#manipulate the prediction to get classification
 	
+	classification = 'trash'
 	if classification == 'trash':
 		ser.write('0')
 	else:
